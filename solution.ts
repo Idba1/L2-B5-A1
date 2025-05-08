a7b9e2b63c30647b9e60ddda090407648d1ace45
Idba1 <moniraislamself@gmail.com>
2025-05-08 23:15:02 +0600

    update problem-4

--- solution.ts
+++ solution.ts
@@ -31,8 +31,8 @@ class Vehicle {
         this.year = year;
     }
 
-    public getInfo(): string {
-        return `Make: ${this.make}, Year: ${this.year}`
+    public getInfo(): void {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
     }
 }
 class Car extends Vehicle {
@@ -43,8 +43,8 @@ class Car extends Vehicle {
         this.model = model;
     }
 
-    public getModel(): string {
-        return `Model: ${this.model}`
+    public getModel(): void {
+        console.log(`Model: ${this.model}`);
     }
 }
 
